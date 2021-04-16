83b1ca6a2032b133a9c38fbb2c7419ba108f5000
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-04-16 16:58:33 +0100

    Finished tests and solution itself

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 
-function App({initialList}) {
+function App({initialList = []}) {
+  const [displayedList, updateDisplayedList] = useState(initialList)
+
+  const removeListElement = clickedIndex => {
+    let updatedDisplayedList = [...displayedList];
+    updatedDisplayedList = updatedDisplayedList.filter((li, index) => {
+      return clickedIndex !== index
+    })
+
+    updateDisplayedList(updatedDisplayedList)
+  }
+
+
   return (
     <section className='container'>
       <ul data-testid='list'>
-
+        {displayedList.map((li, index) => {
+          return (
+            <li 
+            key={`list-element-${index}`} 
+            data-testid={`list-element-${index}`}>
+              {li}
+              <button 
+                data-testid={`remove-${index}`}
+                onClick={() => removeListElement(index)}
+                className="btn btn--danger"
+              >
+                Remove
+              </button>
+            </li>
+            
+          )
+        })}
       </ul>
     </section>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,64 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-describe('Render list elements correctly, when valid list is provided', () => {
-  const initialList = ['This is', 'sample', 'list element'];
+describe('Renders list elements correctly, when valid list is provided', () => {
+  const validList = ['This is', 'sample', 'list element'];
+
+  beforeEach(() => render(<App initialList={validList} />));
 
   test('Render valid list', () => {
-    render(<App />);
     const list = screen.getByTestId('list');
     expect(list).toBeInTheDocument(list);
-    
   });
+
+  test('Render valid list element', () => {
+    const listElements = screen.getAllByTestId(/list-element-/);
+    expect(listElements.length).toEqual(validList.length);
+  });
+
+  test('Render valid list element buttons', () => {
+    const listElementsButtons = screen.getAllByTestId(/remove-/);
+    expect(listElementsButtons.length).toEqual(validList.length);
+  });
+})
+
+describe('Render component and list element "ul" even if a list has not been passed', () => {
+
+  beforeEach(() => {
+    render(<App />);
+  })
+  test('Render valid list', () => {
+    const listElements = screen.getByTestId('list');
+    expect(listElements).toBeInTheDocument(listElements);
+  });
+})
+
+describe('Removes list element from list after "Remove" button was clicked', () => {
+    test('Removes element from an array of nonunique elements', () => {
+      const listOfNonUniqueElements = ['This is', 'sample', 'list element', 'This is'];
+      render(<App initialList={listOfNonUniqueElements} />);
+
+      const buttonElement = screen.getByTestId('remove-3');
+      fireEvent.click(buttonElement);
+
+      const listElements = screen.getAllByTestId(/list-element-/);
+      const listElementsContent = listElements.map(listElem => listElem.textContent.replace('Remove', ''))
+
+    expect(listElementsContent).toEqual(['This is', 'sample', 'list element']);
+    });
+
+    test('Removes element from an array of nunique elements', () => {
+      const listOfUniqueElements = ['This is', 'sample', 'list element'];
+      render(<App initialList={listOfUniqueElements} />);
+
+      const buttonElement = screen.getByTestId('remove-2');
+      fireEvent.click(buttonElement);
+
+      const listElements = screen.getAllByTestId(/list-element-/);
+      const listElementsContent = listElements.map(listElem => listElem.textContent.replace('Remove', ''))
+
+    expect(listElementsContent).toEqual(['This is', 'sample']);
+    });
 })
 
 
